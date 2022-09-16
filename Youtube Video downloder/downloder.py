77f095f73aa25579fbc00959766ab28f7242727f
jahirmj <jahirmj2001@gmail.com>
2022-09-16 08:01:55 +0530

    Add playlist download method

--- Youtube Video downloder/downloder.py
+++ Youtube Video downloder/downloder.py
@@ -1,17 +1,34 @@
 # pip install pytube
 
+from pytube import Playlist
 from pytube import YouTube
-from sys import argv
 
-# link = argv[1]
 link = input("Paste Your Link Here : ")
 
 yt = YouTube(link)
 
 print("Title: ", yt.title)
-
 print("Views: ", yt.views)
+print("Thumbnail-Url: ", yt.thumbnail_url)
 
-yd = yt.streams.get_highest_resolution()
+videos = yt.streams.all()
+vid = list(enumerate(videos))
+for i in vid:
+    print(i)
+print()
+strm = int(input('Enter Index No: '))
+print("Downloading....")
+videos[strm].download('/Users/Family/Downloads/Youtube')
+print("Complete")
 
-yd.download('/Users/Family/Downloads/Youtube')
+
+# yd = yt.streams.get_highest_resolution()
+# yd.download('/Users/Family/Downloads/Youtube')
+# print("Complete")
+# exit()
+
+# ***** Playlist *****
+py = Playlist(input('Enter Playlist Link: '))
+print(f'Downloading : { py.title } ')
+for video in py.videos:
+    video.streams.first().download()
